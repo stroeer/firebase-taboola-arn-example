296b06a6738de1f11caecce73f4c2b595319ba5e
Lukas Bombach <lukas.bombach@googlemail.com>
2024-09-11 14:32:59 +0200

    added native taboola

--- packages/expo-taboola/android/src/main/java/expo/modules/taboola/ExpoTaboolaModule.kt
+++ packages/expo-taboola/android/src/main/java/expo/modules/taboola/ExpoTaboolaModule.kt
@@ -1,44 +1,24 @@
 package expo.modules.taboola
 
+import android.util.Log
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import com.taboola.android.TBLPublisherInfo
+
+const val TABOOLA_PUBLISHER_ID = "stroeer-tonlineandroidapp2022"
 
 class ExpoTaboolaModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
   override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ExpoTaboola')` in JavaScript.
     Name("ExpoTaboola")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
-
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
+    Function("initTaboola") {
+      val publisherInfo = TBLPublisherInfo(TABOOLA_PUBLISHER_ID)
+      Taboola.init(publisherInfo)
+      Log.i("taboola", "toboola init successful")
     }
 
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
+    // todo: remove custom view code
     View(ExpoTaboolaView::class) {
-      // Defines a setter for the `name` prop.
       Prop("name") { view: ExpoTaboolaView, prop: String ->
         println(prop)
       }
--- packages/expo-taboola/src/index.ts
+++ packages/expo-taboola/src/index.ts
@@ -1,26 +1,9 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+import ExpoTaboolaModule from "./ExpoTaboolaModule";
+import ExpoTaboolaView from "./ExpoTaboolaView";
+import { ChangeEventPayload, ExpoTaboolaViewProps } from "./ExpoTaboola.types";
 
-// Import the native module. On web, it will be resolved to ExpoTaboola.web.ts
-// and on native platforms to ExpoTaboola.ts
-import ExpoTaboolaModule from './ExpoTaboolaModule';
-import ExpoTaboolaView from './ExpoTaboolaView';
-import { ChangeEventPayload, ExpoTaboolaViewProps } from './ExpoTaboola.types';
-
-// Get the native constant value.
-export const PI = ExpoTaboolaModule.PI;
-
-export function hello(): string {
-  return ExpoTaboolaModule.hello();
-}
-
-export async function setValueAsync(value: string) {
-  return await ExpoTaboolaModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(ExpoTaboolaModule ?? NativeModulesProxy.ExpoTaboola);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
+export function initTaboola(): void {
+  return ExpoTaboolaModule.initTaboola();
 }
 
 export { ExpoTaboolaView, ExpoTaboolaViewProps, ChangeEventPayload };
