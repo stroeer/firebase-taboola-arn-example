477b82205d5c44e592bdbc4ddc4a7578aa07542e
Lukas Bombach <lukas.bombach@googlemail.com>
2024-09-12 12:29:18 +0200

    made taboola work in the webview

--- packages/expo-taboola/android/src/main/java/expo/modules/taboola/ExpoTaboolaView.kt
+++ packages/expo-taboola/android/src/main/java/expo/modules/taboola/ExpoTaboolaView.kt
@@ -11,14 +11,29 @@ import expo.modules.kotlin.views.ExpoView
 
 class ExpoTaboolaView(context: Context, appContext: AppContext) : ExpoView(context, appContext) {
   internal val webView = WebView(context).also {
+
+    it.settings.javaScriptEnabled = true
+    it.settings.domStorageEnabled = true
+    it.settings.databaseEnabled = true
+
     it.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-    it.webViewClient = object : WebViewClient() {}
-
-
-    val tblWebPage = Taboola.getWebPage().build(it, object : TBLWebListener() {})
-
+    it.webViewClient = object : WebViewClient() {
+      override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+        val extraHeaders = mapOf("view-mode" to "radio-android")
+        view.loadUrl(url, extraHeaders)
+        return true
+      }
+    }
     addView(it)
 
-    it.loadUrl("https://www.t-online.de/")
+    val tblWebPage = Taboola.getWebPage()
+    val tblWebUnit = tblWebPage.build(it, object : TBLWebListener() {})
+    val extraProperties = hashMapOf("darkMode" to "true")
+    tblWebUnit.setUnitExtraProperties(extraProperties)
+
+    WebView.setWebContentsDebuggingEnabled(true)
+
+    val extraHeaders = mapOf("view-mode" to "radio-android")
+    it.loadUrl("https://www.t-online.de/native/radio-android/", extraHeaders)
   }
 }
